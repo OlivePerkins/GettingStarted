abde959775ea4ea3baa52fc07c820c40ac3d04a4
Annclare Perkins <perkinsa@gslc.qld.edu.au>
2019-07-29 18:03:53 +1000

    Add project files.

---
+++ GettingStarted/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace GettingStarted
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //print 'Hello World' to the output window
+            Console.WriteLine("Hello World!");
+
+            //keeps the console clean and open
+            Console.ReadLine();
+      
+    }
+}
+}
